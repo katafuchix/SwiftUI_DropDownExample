373c7ca498dea6636d3bc3f2ab71980a72806805
katafuchix <katafuchix@gmail.com>
2023-03-18 17:17:45 +0900

    Initial Commit

---
+++ DropDownExample/DropDownExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownExampleApp.swift
+//  DropDownExample
+//
+//  Created by cano on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct DropDownExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
